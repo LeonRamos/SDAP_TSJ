00e095b985e548a1c3ee61fe6076a536b0c2014d
x <leon.ramos@zapopan.tecmm.edu.mx>
2026-05-06 14:05:09 -0600

    update config

--- backend/src/core/db/index.js
+++ backend/src/core/db/index.js
@@ -1,8 +1,12 @@
 // backend/src/core/db/index.js
+
 const { Pool } = require('pg');
 
+const connectionString =
+  process.env.DATABASE_URL || 'postgres://tsj_user:tsj_pass@db:5432/tsj_mental_health';
+
 const pool = new Pool({
-  connectionString: 'postgres://tsj_user:tsj_pass@localhost:5432/tsj_mental_health'
+  connectionString
 });
 
 function query(text, params) {
